0f5c8e7875a4563382390423dd611e1dfacb55e0
alisyihab <ali.syihab@gmail.com>
2022-02-15 22:45:15 +0700

    Updates

---
+++ precache-manifest.655a17279417a0b395a5806bceec5fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4435235c383fbd16198d5199f5409807",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "c2113e60a4c53b3a1910",
+    "url": "/portofolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "db6058fa7c711b7ac500",
+    "url": "/portofolio/static/css/main.014ab826.chunk.css"
+  },
+  {
+    "revision": "c2113e60a4c53b3a1910",
+    "url": "/portofolio/static/js/2.0efb7e40.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portofolio/static/js/2.0efb7e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6058fa7c711b7ac500",
+    "url": "/portofolio/static/js/main.58bf72d2.chunk.js"
+  },
+  {
+    "revision": "a3a6c8a1f81f721d2b09",
+    "url": "/portofolio/static/js/runtime-main.cef59536.js"
+  }
+]);
